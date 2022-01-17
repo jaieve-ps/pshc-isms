7b4179af1d3fdee68b9d7f73a2648cfac0a3c35e
jaieve-ps <jaieve1469@polestarhc.com>
2022-01-17 13:37:11 +0900

    [feat] ADD mapper.xml 예제파일 추가

---
+++ src/main/java/com/pshc/isms/mapper/estimate/EstimateMapper.java
@@ -0,0 +1,13 @@
+package com.pshc.isms.mapper.estimate;
+
+import com.pshc.isms.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface EstimateMapper {
+
+
+}
